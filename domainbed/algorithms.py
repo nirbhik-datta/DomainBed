0e35523707a91f82e01a6a5d864a0fd85a60d3a5
Das, Barun <bdas31@gatech.edu>
2022-10-06 17:16:19 -0400

    Added VRexx + RSC algorithm
    - Combined VRex and RSC
    - Need to test on dataset

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -920,6 +920,80 @@ class RSC(ERM):
 
         return {'loss': loss.item()}
 
+class VRex_RSC(ERM):
+    def init(self, input_shape, num_classes, num_domains, hparams):
+        super(VRex_RSC, self).__init__(input_shape, num_classes, num_domains,
+                                  hparams)
+        self.register_buffer('update_count', torch.tensor([0]))
+        self.drop_f = (1 - hparams['rsc_f_drop_factor']) * 100
+        self.drop_b = (1 - hparams['rsc_b_drop_factor']) * 100
+        self.num_classes = num_classes
+
+    def update(self, minibatches, unlabeled=None):
+        device = "cuda" if minibatches[0][0].is_cuda else "cpu"
+
+        # inputs
+        all_x = torch.cat([x for x, y in minibatches])
+        # labels
+        all_y = torch.cat([y for _, y in minibatches])
+        # one-hot labels
+        all_o = torch.nn.functional.one_hot(all_y, self.num_classes)
+        # features
+        all_f = self.featurizer(all_x)
+        # predictions
+        all_p = self.classifier(all_f)
+        # VRex penalty
+        if self.update_count >= self.hparams["vrex_penalty_anneal_iters"]:
+            penalty_weight = self.hparams["vrex_lambda"]
+        else:
+            penalty_weight = 1.0
+
+
+        # Equation (1): compute gradients with respect to representation
+        all_g = autograd.grad((all_p * all_o).sum(), all_f)[0]
+
+        # Equation (2): compute top-gradient-percentile mask
+        percentiles = np.percentile(all_g.cpu(), self.drop_f, axis=1)
+        percentiles = torch.Tensor(percentiles)
+        percentiles = percentiles.unsqueeze(1).repeat(1, all_g.size(1))
+        mask_f = all_g.lt(percentiles.to(device)).float()
+
+        # Equation (3): mute top-gradient-percentile activations
+        all_f_muted = all_f * mask_f
+
+        # Equation (4): compute muted predictions
+        all_p_muted = self.classifier(all_f_muted)
+
+        # Section 3.3: Batch Percentage
+        all_s = F.softmax(all_p, dim=1)
+        all_s_muted = F.softmax(all_p_muted, dim=1)
+        changes = (all_s * all_o).sum(1) - (all_s_muted * all_o).sum(1)
+        percentile = np.percentile(changes.detach().cpu(), self.drop_b)
+        mask_b = changes.lt(percentile).float().view(-1, 1)
+        mask = torch.logical_or(mask_f, mask_b).float()
+
+        # Equations (3) and (4) again, this time mutting over examples
+        all_p_muted_again = self.classifier(all_f * mask)
+
+        # Equation (5): update
+        loss = F.cross_entropy(all_p_muted_again, all_y)
+        penalty = ((loss - loss.mean()) ** 2).mean()
+        loss = loss + penalty_weight * penalty
+        if self.update_count == self.hparams['vrex_penalty_anneal_iters']:
+            # Reset Adam (like IRM), because it doesn't like the sharp jump in
+            # gradient magnitudes that happens at this step.
+            self.optimizer = torch.optim.Adam(
+                self.network.parameters(),
+                lr=self.hparams["lr"],
+                weight_decay=self.hparams['weight_decay'])
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.update_count += 1
+
+        return {'loss': loss.item(), 'penalty': penalty.item()}
 
 class SD(ERM):
     """
